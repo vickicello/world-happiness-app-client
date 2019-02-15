226d73078111b90c726fe42c0cb4a81295461789
vickicello <vicki.cello@gmail.com>
2019-02-15 18:51:18 -0500

    successfully hooked up rails api

--- src/App.js
+++ src/App.js
@@ -3,33 +3,29 @@ import Countries from './components/Countries';
 import CountryService from './services/CountryService';
 import './App.css';
 
-let countries = [
-  { country: 'France', id: 1 },
-  { country: 'Peru', id: 2 },
-  { country: 'USA', id: 4 }
-]
 class App extends Component {
-  contructor() {
-    super() 
-      this.state = {
-        countries: []
-      }
+  constructor () {
+    super ();
 
-    componentDidMount() {
-      CountryService.fetchCountries().then(countries => this.setState({ countries }))
+    this.state = {
+      countries: []
     }
   }
 
+
+  componentDidMount() {
+    CountryService.fetchCountries().then(countries => this.setState({ countries }));
+  }
+
   render() {
+    console.log(this.state.countries);
     return (
       <div className="App">
         <div className="navbar">
-          <p>Check out my navbar!</p>
           {/* <Navbar /> */}
         </div>
         <div className="main-content">
-          <p>Check out my main content!</p>
-          <Countries countries={countries}/>
+          <Countries countries={this.state.countries} />
         </div>
       </div>
     );
--- src/services/CountryService.js
+++ src/services/CountryService.js
@@ -1,6 +1,8 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
 const CountryService = {
   fetchCountries: () => {
-    return fetch('/countries')
+    return fetch(`${API_URL}/countries`)
     .then(response => response.json())
   }
 }
