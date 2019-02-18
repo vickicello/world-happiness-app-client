7ec670c25d59c4523cf1d473c5bb6ebbd1f0ad9e
vickicello <vicki.cello@gmail.com>
2019-02-18 17:07:10 -0500

    turn country container into table, with some css styling

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 const Countries = ({countries}) => {
   const renderCountries = countries.map(country =>
-    <p key={country.id}>{country.country}</p>
-
+    <tr>
+      <td key={country.id}>{country.happiness_rank}</td>
+      <td>{country.country}</td>
+      <td>{country.happiness_score}</td>
+    </tr>
   )
 
   return (
     <div>
+      <h1>World Happiness Rankings</h1>
+        <table className="countryTable">
+        <tr>
+          <th>Rank</th>
+          <th>Country</th>
+          <th>Happiness Score</th>
+        </tr>
       {renderCountries}
+      </table>
     </div>
   )
 }
